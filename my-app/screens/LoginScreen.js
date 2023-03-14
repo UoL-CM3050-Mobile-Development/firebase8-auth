0bb57a4916d3aef1beb15024a0e99c4adcba1eac
Volodymyr Kurov <vokur13@gmail.com>
2023-03-14 16:52:08 +0200

    Update LoginScreen.js

--- my-app/screens/LoginScreen.js
+++ my-app/screens/LoginScreen.js
@@ -6,22 +6,25 @@ import {
   TouchableOpacity,
   View,
 } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <KeyboardAvoidingView style={styles.container} behavior="padding">
       <View style={styles.inputContainer}>
         <TextInput
           placeholder="Email"
-          //   value={ }
-          //   onChangeText={(text) => { }}
+          value={email}
+          onChangeText={(text) => setEmail(text)}
           style={styles.input}
         />
         <TextInput
           placeholder="Password"
-          //   value={ }
-          //   onChangeText={(text) => { }}
+          value={password}
+          onChangeText={(text) => setPassword(text)}
           style={styles.input}
           secureTextEntry
         />
